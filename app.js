0663314c5666f89f38c279db0cce6371f8626f5d
David Morisset <dmorisset@gmail.com>
2019-05-22 10:44:45 +0100

    added an error if the path is not correct

--- app.js
+++ app.js
@@ -9,25 +9,29 @@ httpserver.on('request', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   let url_path = url.parse(req.url, true).path;
-  let query = url_path.split(":")[1].slice(0, -1);
-
-  let day = new Date(query),
-      date = null;
-
-  if (isNaN(day.getTime())) {
-    day = new Date(parseInt(query));
-  }
-
-  if (isNaN(day.getTime())) {
-    date = {error: day.toUTCString()};
+  if (url_path.indexOf(":") === -1) {
+    res.end(JSON.stringify({error: "address not valid"}));
   } else {
-    date = {
-      unix: day.getTime(),
-      utc: day.toUTCString()
-    }
-  }
+    let query = url_path.split(":")[1].slice(0, -1);
 
-  res.end(JSON.stringify(date));
+    let day = new Date(query),
+        date = null;
+
+    if (isNaN(day.getTime())) {
+      day = new Date(parseInt(query));
+    }
+
+    if (isNaN(day.getTime())) {
+      date = {error: day.toUTCString()};
+    } else {
+      date = {
+        unix: day.getTime(),
+        utc: day.toUTCString()
+      }
+    }
+
+    res.end(JSON.stringify(date));
+  }
 });
 
 let port = process.env.PORT || 3000;
